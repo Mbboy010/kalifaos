05f7f8db7cf046637eb8598c07be6b0fdf2e2677
mbboy010 <mbboy010@gmail.com>
2025-07-28 09:51:33 +0100

    Initial commit

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,15 @@
 // app/not-found.tsx
+"use client"
 import NotFou from "../components/not/NotFound"
-
+import Head from 'next/head';
 
 export default function NotFound() {
   return (
     <>
+    <Head>
+        <title>404 - Page Not Found</title>
+        <meta name="description" content="The page you are looking for does not exist." />
+      </Head>
       <div className="container mx-auto min-h-full">
           <NotFou />
       </div>

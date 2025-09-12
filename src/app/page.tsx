1230fb3abb978b6dead9b7944d047b561379d1db
mbboy010 <mbboy010@gmail.com>
2025-09-12 16:08:50 +0100

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import HomeClient from '../components/home/HomeClient'
 
 export const metadata = {
-  title: 'Home - Kalifa Os',
+  title: 'Kalifa Os',
   description: 'Discover Kalifa OS, the next-generation operating system. Unlock your device with FRP bypass tools and system app management. Try our free diagnostic service today',
 }
 

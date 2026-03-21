3f158ca11476855e698d1efc6e1506d470012136
mbboy010 <mbboy010@gmail.com>
2026-03-21 17:20:50 +0100

    Initial commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/sserver';
+import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
